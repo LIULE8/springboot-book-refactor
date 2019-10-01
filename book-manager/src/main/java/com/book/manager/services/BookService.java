614e1fb0d9f24153bd8f9aae079bf6dfeef7b5cc
Leo Liu <scau_lzc@163.com>
2019-10-01 22:57:51 +0800

    add bookService interface

---
+++ book-manager/src/main/java/com/book/manager/services/BookService.java
@@ -0,0 +1,4 @@
+package com.book.manager.services;
+
+public interface BookService {
+}
---
+++ book-manager/src/main/java/com/book/manager/services/impl/BookServiceImpl.java
@@ -0,0 +1,10 @@
+package com.book.manager.services.impl;
+
+import com.book.manager.services.BookService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class BookServiceImpl implements BookService {
+}
